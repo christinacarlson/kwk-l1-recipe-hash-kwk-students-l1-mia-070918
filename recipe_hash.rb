223a4900fde8254892e0d0fea5083996e6309979
christinacarlson <christina.carlson213@gmail.com>
2018-07-12 14:02:21 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -20,8 +20,8 @@ def amount_of_chocolate_chips(perfect_10_recipe)
 end
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
-def ingredients_and_amounts(perfect_10_recipe).each do |recipe|
-  puts perfect_10_recipe
+def ingredients_and_amounts(perfect_10_recipe).each do |ing, amt|
+  puts perfect_10_recipe[ing]=amt
   end
 end
 
